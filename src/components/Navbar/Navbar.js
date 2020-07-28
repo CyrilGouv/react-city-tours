372ea95d3a69a9accbb0be223ee02678af107dcd
Cyril Gouverneur <cyril@imac-de-cyril.home>
2020-07-28 16:23:03 +0200

    Init Project

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from '../../logo.svg'
+import './navbar.scss'
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <img src={ logo } alt="Logo city Tours Company"/>
+            <ul className="nav-links">
+                <li>
+                    <a className="nav-link" href="/">Home</a>
+                </li>
+                <li>
+                    <a className="nav-link" href="/">About</a>
+                </li>
+                <li>
+                    <a className="nav-link active" href="/">Tours</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
